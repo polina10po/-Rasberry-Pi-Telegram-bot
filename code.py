6b81b17dd9b3a4882fb415097a59fe6a54eb2019
Polina <65661088+polina10po@users.noreply.github.com>
2022-12-19 17:24:57 +0500

    Update code.py

--- code.py
+++ code.py
@@ -5,37 +5,28 @@ from telepot.loop import MessageLoop
 from picamera import PiCamera
 
 now = datetime.datetime.now()
-camera = PiCamera();   
+camera = PiCamera()
 
 def handle(msg):
-  chat_id = msg['chat']['id']
-  command = msg['text']
-  
-  print('Received:')
-  print(command)
-  
-  if command == '/hi':
-    bot.sendMessage(chat_id, "Hi!")
-  elif command == '/time':
-    bot.sendMessage(chat_id, "Time:" + str(now.hour) + ":" + str(now.minute))
-  elif command == '/date':
-    bot.sendMessage(chat_id, "Date:" + str(now.day) + "/" + str(now.month) + "/" + str(now.year))
-  elif command == '/image':   
-    camera.start_preview()   
-    camera.capture('/home/orangepi/img.jpg',resize=(640,480))   
-    time.sleep(2)   
-    camera.stop_preview()   
-    camera.close()   
-    bot.sendPhoto(chat_id, open('/home/orangepi/img.jpg', 'rb'))
-    bot.sendMessage(chat_id, str("Time:") + str(now.hour) + str(":") + str(now.minute))  
-  elif command == '/file':
-    bot.sendMessage(chat_id, document = open('/home/polina/bot.py'))
-    
-bot = telepot.Bot('5790505945:AAGf0gTk8LD3k4IXhdQog5YQezat2YyTFZQ')
-print(bot.getMe())
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    if command == '/hi':
+        bot.sendMessage(chat_id, "HI")
+    elif command == '/date':
+        bot.sendMessage(chat_id, "Date:" + str(now.day) + "." + str(now.month) + "." + str(now.year))
+    elif command == '/time':
+        bot.sendMessage(chat_id, "Time:" + str(now.hour) + ":" + str(now.minute))
+    elif command == '/file':
+        bot.sendDocument(chat_id, document = open('/home/orangepi/bot.py'))
+    elif command == '/image':
+        camera.start_preview()
+        camera.capture('/home/orangepi/img.jpg', resize(640, 480))
+        camera.stop_preview()
+        camera.close()
+        bot.sendPhoto(chat_id, open('/home/orangepi/img.jpg'))
 
-MessegeLoop(bot, handle).run_as_thread()
-print('Listening...')
+bot = telepot.Bot('5790505945:AAGf0gTk8LD3k4IXhdQog5YQezat2YyTFZQ')
+MessageLoop(bot, handle).run_as_thread()
 
 while 1:
-  time.sleep(10)
+    time.sleep(10)
